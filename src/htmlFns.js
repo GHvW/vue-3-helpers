8bbecf52a0fb26997745db251234123b9cbc7f1c
Garrett van Wageningen <ghvanw283@gmail.com>
2021-09-03 14:30:29 -0500

    save

---
+++ src/htmlFns.js
@@ -0,0 +1,50 @@
+import { h } from "vue";
+
+const div = h.bind(null, "div");
+
+const p = h.bind(null, "p");
+
+const ul = h.bind(null, "ul");
+
+const ol = h.bind(null, "ol");
+
+const li = h.bind(null, "li");
+
+const span = h.bind(null, "span");
+
+const img = h.bind(null, "img");
+
+const section = h.bind(null, "section");
+
+const a = h.bind(null, "a");
+
+const table = h.bind(null, "table");
+
+const tbody = h.bind(null, "tbody");
+
+const td = h.bind(null, "td");
+
+const tfoot = h.bind(null, "tfoot");
+
+const th = h.bind(null, "th");
+
+const thead = h.bind(null, "thead");
+
+const tr = h.bind(null, "tr");
+
+const h1 = h.bind(null, "h1");
+
+const h2 = h.bind(null, "h2");
+
+const h3 = h.bind(null, "h3");
+
+const h4 = h.bind(null, "h4");
+
+const h5 = h.bind(null, "h5");
+
+const h6 = h.bind(null, "h6");
+
+
+export {
+    div, p, li, ul, ol, span, img, section, a, table, tbody, td, tfoot, th, thead, tr, h1, h2, h3, h4, h5, h6,
+}
